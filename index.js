e156ddd368b31599e37194a68879b4c3508b627b
Ahmed Kamel <ak@ahmedkamel.net>
2016-08-19 00:14:33 -0700

    enable https

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var Promise       = require('bluebird')
   , Relay         = require('taskmill-core-relay').Relay
   , parser        = require('./lib/parser')
   , ua            = require('universal-analytics')
+  , http          = require('http')
+  , https         = require('https')
   ;
 
 var app = express();
@@ -33,43 +35,56 @@ app.get('/', (req, res) => {
 
 app.all('/*', (req, res, next) => {
   // todo [akamel] this doesn't take port nto account
-  var route = parser.parse(req.hostname, req.path);
-  if (route) {
-    cors({
-        exposedHeaders : ['x-request-id']
-    })(req, res, () => {
-      if (req.method === 'OPTIONS') {
-        res.end();
-        return;
+  Promise
+    .try(() => {
+      return parser.parse(req.hostname, req.path);
+    })
+    .then((route) => {
+      if (!route) {
+        return res.end();
       }
 
-      req.visitor.event('Gateway', 'Run', req.hostname, req.url, { p : req.url }).send();
-
-      var data = {
-        codedb_url  : config.getUrl('codedb')
-      };
-
-      var blob = req.get('blob');
-      if (blob) {
-        data.blob = new Buffer(blob, 'base64').toString('utf8');
-      }
-
-      var ttl_id = req.get('tty.id');
-      if (ttl_id) {
-        data.tty = {
-            ws : config.getUrl('tty')
-          , id : ttl_id
+      cors({
+          exposedHeaders : ['x-request-id']
+      })(req, res, () => {
+        if (req.method === 'OPTIONS') {
+          res.end();
+          return;
         }
-      }
 
-      _.extend(data, route);
+        req.visitor.event('Gateway', 'Run', req.hostname, req.url, { p : req.url }).send();
 
-      relay.emit(data, req, res);
-    });
-    
-  } else {
-    res.end();
-  }
+        var data = {
+          codedb_url  : config.getUrl('codedb')
+        };
+
+        var blob = req.get('blob');
+        if (blob) {
+          data.blob = new Buffer(blob, 'base64').toString('utf8');
+        }
+
+        var ttl_id = req.get('tty.id');
+        if (ttl_id) {
+          data.tty = {
+              ws : config.getUrl('tty')
+            , id : ttl_id
+          }
+        }
+
+        _.extend(data, route);
+
+        // todo [akamel] for some reason we need to disable .end for https to work...
+        res.end = () => {
+          // console.trace('end');
+        }
+ 
+        relay.emit(data, req, res);
+      });
+    })
+    .catch((err) => {
+      winston.error(err);
+      res.end();
+    })
 });
 
 function main() {
@@ -79,10 +94,39 @@ function main() {
       winston.info('taskmill-core-gateway [relay] :http://localhost:%d', '0');
     });
 
-  app.listen(config.get('www.port'), function() {
-    let port = this.address().port;
-    winston.info('taskmill-core-gateway [started] :http://localhost:%d', port);
-  });
+  // app.listen(config.get('www.port'), function() {
+  //   let port = this.address().port;
+  //   winston.info('taskmill-core-gateway [started] :http://localhost:%d', port);
+  // });
+
+  if (config.has('gateway.ssl.github.cert')) {
+    let https_server = https.createServer({
+                          key   : config.get('gateway.ssl.github.key')
+                        , cert  : config.get('gateway.ssl.github.cert')
+                        , ca    : config.has('gateway.ssl.github.ca')? config.get('gateway.ssl.github.ca') : undefined
+                      }, app);
+
+    https_server.listen(config.get('gateway.port'), function() {
+      let port = this.address().port;
+      winston.info('taskmill-core-gateway [started] :https://localhost:%d', port);
+    });
+
+    let http_server = http.createServer((req, res) => {
+      res.end();
+    });
+
+    http_server.listen(config.get('gateway.port') + 1, function() {
+      let port = this.address().port;
+      winston.info('taskmill-core-gateway [started] :http://localhost:%d', port);
+    });
+  } else {
+    let http_server = http.createServer(app);
+
+    http_server.listen(config.get('gateway.port'), function() {
+      let port = this.address().port;
+      winston.info('taskmill-core-gateway [started] :http://localhost:%d', port);
+    });
+  }
 }
 
 main();
