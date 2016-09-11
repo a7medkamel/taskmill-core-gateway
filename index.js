ed6cb41cd0b5c6739984df1cbfeabf4c5f34237c
Ahmed Kamel <ak@ahmedkamel.net>
2016-09-11 00:00:14 -0400

    report on connected agents & cleanup

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ process.on('uncaughtException', function (err) {
   console.error(err.stack || err.toString());
 });
 
+// NOTE: event name is camelCase as per node convention
+// process.on("unhandledRejection", function(reason, promise) {
+//     // See Promise.onPossiblyUnhandledRejection for parameter documentation
+//     console.log(reason, promise);
+// });
+
 function main() {
   return http
           .listen({ port : config.getUrlObject('gateway').port })
